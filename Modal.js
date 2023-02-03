2f0e849d9e6b4c5b93c0a349f1f7c84d81ea8fab
Besufekad Alemu <besurye@gmail.com>
2023-02-03 15:10:30 +0300

    add mobile and desktop object for the array to iterate through

---
+++ Modal.js
@@ -0,0 +1,256 @@
+// Objects for storing the mobile and desktop projects
+
+const deskCards = [
+  {
+    title: "Tonic",
+    technology: ["Canopy", "Back End Dev", 2015],
+    images: "./images/DESKTOP IMAGES/Snapshoot Portfolio.png",
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    textPopup:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ratione corporis, neque, quae totam incidunt ut veniam estnobis sint delectus aliquid aut minima consequatur praesentium nemo impedit dolorum aspernatur.",
+    langPopup: ["html", "css", "javaScript", "github", "ruby", "Bootstaps"],
+    live: "https://mj-e-boc.github.io/",
+    source: "https://github.com/mj-e-boc/Ronald-Portfolio",
+    closeBtn: "&times;",
+    languages: ["html", "css", "javaScript"],
+  },
+  {
+    title: "Multi-Post Stories",
+    technology: ["FACEBOOK", "Full Stack Dev", 2015],
+    images: "./images/DESKTOP IMAGES/Snapshoot Portfolio-1.png",
+    text: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
+    textPopup:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ratione corporis, neque, quae totam incidunt ut veniam estnobis sint delectus aliquid aut minima consequatur praesentium nemo impedit dolorum aspernatur.",
+    langPopup: ["html", "css", "javaScript", "github", "ruby", "Bootstaps"],
+    live: "https://mj-e-boc.github.io/",
+    source: "https://github.com/mj-e-boc/Ronald-Portfolio",
+    closeBtn: "&times;",
+    languages: ["html", "Ruby on rails", "css", "javaScript"],
+  },
+  {
+    title: "Facebook 360",
+    technology: ["FACEBOOK", "Full Stack Dev", 2015],
+    images: "./images/DESKTOP IMAGES/Snapshoot Portfolio-2.png",
+    text: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    textPopup:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ratione corporis, neque, quae totam incidunt ut veniam estnobis sint delectus aliquid aut minima consequatur praesentium nemo impedit dolorum aspernatur.",
+    langPopup: ["html", "css", "javaScript", "github", "ruby", "Bootstaps"],
+    live: "https://mj-e-boc.github.io/",
+    source: "https://github.com/mj-e-boc/Ronald-Portfolio",
+    closeBtn: "&times;",
+    languages: ["html", "Ruby on rails", "css", "javaScript"],
+  },
+  {
+    title: "Uber Navigation",
+    technology: ["Uber", "Lead Developer", 2018],
+    images: "./images/DESKTOP IMAGES/Snapshoot Portfolio-3.png",
+    text: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+    textPopup:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ratione corporis, neque, quae totam incidunt ut veniam estnobis sint delectus aliquid aut minima consequatur praesentium nemo impedit dolorum aspernatur.",
+    languages: ["html", "Ruby on rails", "css", "javaScript"],
+    langPopup: ["html", "css", "javaScript", "github", "ruby", "Bootstaps"],
+    live: "https://mj-e-boc.github.io/",
+    source: "https://github.com/mj-e-boc/Ronald-Portfolio",
+    closeBtn: "&times;",
+  },
+];
+
+// storing data for mobile part
+
+const mobCards = [
+  {
+    title: "Tonic",
+    technology: ["Canopy", "Back End Dev", 2015],
+    images: "/images/projectsimages/Snapshoot Portfolio.png",
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    textPopup:
+      "  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus.",
+    closeBtn: "&times;",
+    languages: ["html", "css", "javaScript"],
+    live: "https://mj-e-boc.github.io/",
+    source: "https://github.com/mj-e-boc/Ronald-Portfolio",
+  },
+  {
+    title: "Multi-Post Stories",
+    technology: ["Canopy", "Back End Dev", 2015],
+    images: "./images/projectsimages/Snapshoot Portfolio project 2.png",
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    languages: ["html", "css", "javaScript"],
+    textPopup:
+      "  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus.",
+    closeBtn: "&times;",
+    live: "https://mj-e-boc.github.io/",
+    source: "https://github.com/mj-e-boc/Ronald-Portfolio",
+  },
+  {
+    title: "Tonic",
+    technology: ["Canopy", "Back End Dev", 2015],
+    images: "./images/projectsimages/Snapshoot Portfolio project 3.png",
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required",
+    languages: ["html", "css", "javaScript"],
+    textPopup:
+      "  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus.",
+    closeBtn: "&times;",
+    live: "https://mj-e-boc.github.io/",
+    source: "https://github.com/mj-e-boc/Ronald-Portfolio",
+  },
+  {
+    title: "Multi-Post Stories",
+    technology: ["Canopy", "Back End Dev", 2015],
+    images: "./images/projectsimages/Snapshoot Portfolio project 4.png",
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required",
+    languages: ["html", "css", "javaScript"],
+    textPopup:
+      "  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus.",
+    closeBtn: "&times;",
+    live: "https://mj-e-boc.github.io/",
+    source: "https://github.com/mj-e-boc/Ronald-Portfolio",
+  },
+];
+
+
+// Make sure to add this class names in the HTML.....!!!!!!!!!!!!!!!!!
+
+const desktop = document.querySelector(".desktop");
+const mobile = document.querySelector(".mobile");
+
+// looping through the cards of the desktop to add cards dynamically
+
+for (let i = 0; i < cards.length; i += 1) {
+  let lang = "";
+  cards[i].languages.forEach((cards) => {
+    lang = `${lang}<li class="${cards}">${cards}</li>`;
+  });
+
+  let tech = "";
+  cards[i].technology.forEach((techn) => {
+    tech = `${tech}<li class="${techn}"><span></span>${techn}</li>`;
+  });
+
+  desktop.innerHTML += `
+  <div class="card">
+  <div class="card-img">
+    <img src="${cards[i].images}" alt="tonic project image" />
+  </div>
+  <div class="card-content">
+    <h2 class="card-title">${cards[i].title}</h2>
+    <ul class="card-info">
+      ${tech}
+    </ul>
+    <p class="card-para">${cards[i].text}</p>
+    <ul class="card-lang">
+    ${lang}
+    </ul>
+    <button class="card-btn card-btn-${i}">See Project</button>
+  </div>
+</div>
+`;
+}
+
+// looping through the cards of the mobile to add cards dynamically
+
+for (let i = 0; i < moboCards.length; i += 1) {
+  let lang = "";
+  moboCards[i].languages.forEach((cards) => {
+    lang = `${lang}<li class="${cards}">${cards}</li>`;
+  });
+
+  let tech = "";
+  moboCards[i].technology.forEach((techn) => {
+    tech = `${tech}<li class="${techn}"><span></span>${techn}</li>`;
+  });
+
+  mobile.innerHTML += `
+  <div class="card">
+  <div class="card-content">
+    <h2 class="card-title">${moboCards[i].title}</h2>
+    <ul class="card-info"> 
+    ${tech}
+    </ul>
+    <p class="card-para">${moboCards[i].text}</p>
+    <ul class="card-lang">
+      ${lang}
+    </ul>
+    <button class="card-btn card-${i}">See Project</button>
+  </div>
+  <div class="card-img">
+    <img src="${moboCards[i].images}" alt="tonic project image" />
+    </div>
+    </div
+    `;
+}
+
+// Variable declarations
+
+//  change the class names or ID's here
+
+const cardTitle = document.querySelector("#card-title");
+const cardInfo = document.querySelector("#card-info");
+const cardImg = document.querySelector("#modal-img");
+const cardText = document.querySelector("#card-para");
+const cardLang = document.querySelector("#card-lang");
+const closeBtn = document.querySelector("#close-btn");
+const modal = document.querySelector("#modal");
+const modalLive = document.querySelector("#live-btn");
+const modalSource = document.querySelector("#source-btn");
+
+// Modal for desktop
+
+for (let i = 0; i < cards.length; i += 1) {
+  document.querySelector(`.card-btn-${i}`).addEventListener("click", () => {
+    document.querySelector(".hamburger").style.display = "none";
+
+    modal.style.display = "block";
+
+    let lang = "";
+    cards[i].langPopup.forEach((cards) => {
+      lang = `${lang}<li class="${cards}">${cards}</li>`;
+    });
+
+    let tech = "";
+    cards[i].technology.forEach((techn) => {
+      tech = `${tech}<li class="${techn}"><span></span>${techn}</li>`;
+    });
+
+    cardTitle.innerHTML = `${cards[i].title}`;
+    cardInfo.innerHTML = `${tech}`;
+    cardLang.innerHTML = `${lang}`;
+    cardText.innerHTML = `${cards[i].textPopup}`;
+    cardImg.innerHTML = `<img src="${cards[i].images}">`;
+    modalLive.setAttribute("href", cards[i].live);
+    modalSource.setAttribute("href", cards[i].source);
+  });
+}
+
+// popup for mobile
+for (let i = 0; i < moboCards.length; i += 1) {
+  document.querySelector(`.card-${i}`).addEventListener("click", () => {
+    modal.style.display = "block";
+    document.querySelector(".hamburger").style.display = "none";
+
+    let lang = "";
+    moboCards[i].languages.forEach((cards) => {
+      lang = `${lang}<li class="${cards}">${cards}</li>`;
+    });
+
+    let tech = "";
+    moboCards[i].technology.forEach((techn) => {
+      tech = `${tech}<li class="${techn}"><span></span>${techn}</li>`;
+    });
+
+    cardTitle.innerHTML = `${moboCards[i].title}`;
+    cardInfo.innerHTML = `${tech}`;
+    cardLang.innerHTML = `${lang}`;
+    cardText.innerHTML = `${moboCards[i].textPopup}`;
+    cardImg.innerHTML = `<img src="${moboCards[i].images}">`;
+    modalLive.setAttribute("href", moboCards[i].live);
+    modalSource.setAttribute("href", moboCards[i].source);
+  });
+}
+
+// close button event listener
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+  document.querySelector(".hamburger").style.display = "flex";
+});
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const hamburgerMenu = document.querySelector('.hamburgermenu');
-const hamburger = document.querySelector('.hamburger');
-const bar1 = document.querySelector('.bar1');
-const bar2 = document.querySelector('.bar2');
-const bar3 = document.querySelector('.bar3');
-const modalWindow = document.createElement('div');
+const hamburgerMenu = document.querySelector(".hamburgermenu");
+const hamburger = document.querySelector(".hamburger");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+const modalWindow = document.createElement("div");
 
-modalWindow.className = 'modal-background';
+modalWindow.className = "modal-background";
 document.body.appendChild(modalWindow);
 
-function createModalWindow (projButton){
+function createModalWindow(projButton) {
   `
   <div class="popupwindowIn">
     <div class="header">
@@ -68,239 +68,23 @@ function createModalWindow (projButton){
     </div>
     
   </div>
-`
+`;
 }
 
-hamburger.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('showmenu');
-  hamburger.classList.toggle('hamburgerclicked');
-  bar1.classList.toggle('bar1clicked');
-  bar2.classList.toggle('bar2clicked');
-  bar3.classList.toggle('bar3clicked');
+hamburger.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("showmenu");
+  hamburger.classList.toggle("hamburgerclicked");
+  bar1.classList.toggle("bar1clicked");
+  bar2.classList.toggle("bar2clicked");
+  bar3.classList.toggle("bar3clicked");
 });
 
-
-
-document.querySelectorAll('.hamItem').forEach((item) => item.addEventListener('click', () => {
-  hamburgerMenu.classList.remove('showmenu');
-  bar1.classList.remove('bar1clicked');
-  bar2.classList.remove('bar2clicked');
-  bar3.classList.remove('bar3clicked');
-  hamburger.classList.remove('hamburgerclicked');
-}));
-
-// projects section
-
-
-const projData = 
-[
- project1 = {
-  desktopimg: "images/projectsimages/Snapshoot Portfolio.png",
-mobileimg:'images/DESKTOP IMAGES/Snapshoot Portfolio-1.png',
-  title: 'Tonic',
-  made: ['Canopy', 'Back End Dev', '2015'],
-  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
-languages: ['html','css','javascript'],
-projectbutton:'projectbutton',
-},
- project2 = {
-  desktopimg: "images/projectsimages/Snapshoot Portfolio project 2.png",
-  desktopimgPopup:'',
-mobileimg:'images/DESKTOP IMAGES/Snapshoot Portfolio-2.png',
-  title: 'Multi-Post Stories',
-  made: ['Canopy', 'Back End Dev', '2015'],
-  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
-languages: ['html','css','javascript'],
-projectbutton:'projectbutton',
-},
- project3 = {
-  desktopimg: 'images/DESKTOP IMAGES/Snapshoot Portfolio-3.png',
-  desktopimgPopup:'',
-mobileimg:"images/projectsimages/Snapshoot Portfolio project 3.png",
-  title: 'Facebook 360',
-  made: ['Canopy', 'Back End Dev', '2015'],
-  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
-languages: ['html','css','javascript'],
-projectbutton:'projectbutton',
-},
- project4 = {
-  desktopimg: 'images/DESKTOP IMAGES/Snapshoot Portfolio.png',
-  desktopimgPopup:'',
-  mobileimg:"images/projectsimages/Snapshoot Portfolio project 4.png",
-  title: 'Uber Navigation',
-  made: ['Canopy', 'Back End Dev', '2015'],
-  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
-languages: ['html','css','javascript'],
-projectbutton:'projectbutton',
-}];
-
-function createCard(projectnumber){
-
-    const projectsCards = document.querySelector('.projectscards');
-    let projDiv = document.createElement('div');
-let value = String(projectnumber);
-let s = value.length-1;
-let arr = value.split('');
-if(arr[s]%2===0){
-  projDiv.className = 'project';
-} 
-else{projDiv.className = 'project pro2';
-projectsCards.appendChild(projDiv);} 
-    projDiv.innerHTML = `
-    
-    <div class="img1">
-    <img
-      class="imgAA"
-      src="${projectnumber.mobileimg}"
-      alt="img"
-    />
-    <img
-      class="imgA"
-      src="${projectnumber.desktopimg}"
-      alt="dotimage"
-    />
-  </div>
-  <div class='projectstext'>
-  <h2>${projectnumber.title}</h2>
-  <ul class="qualities">
-     <li class="qitem">${projectnumber.made[0]}</li>
-    <li>
-       <img src="./images/projectsimages/Counter.png" alt="dotimage" />
-    </li>
-    <li class="qitem">${projectnumber.made[1]}</li>
-     <li>
-       <img src="./images/projectsimages/Counter.png" alt="dotimage" />
-    </li>
-    <li class="qitem">${projectnumber.made[2]}</li>
-  </ul>
-   <p>
-  ${projectnumber.description}
-  </p>
-  <ul class="languages">
-    <li>${projectnumber.languages[0]}</li>
-    <li>${projectnumber.languages[1]}</li>
-    <li>${projectnumber.languages[2]}</li>
-  </ul>
-   <button type="button" class="button">See Project</button></div>
-          
-    `
-} 
-
-createCard(project3)
-
-// for(a of projData){
-//   createCard(a);
-// };
-
-// console.log(project1.mobileimg);
-// function createCard(projectnumber) {
-
-//     const projects = document.querySelector('.projectscards');
-//     const projectsCards = document.querySelector('.projectscards');
-//     let projDiv = document.createElement('div');
-//     projDiv.className = 'project pro2';
-//     projectsCards.appendChild(projDiv);
-//     let imgDiv = document.createElement('div');
-//     imgDiv.className = 'img1';
-//     projDiv.appendChild(imgDiv);
-//     const desktopImg = document.createElement('img');
-//     desktopImg.className ='imgAA';
-//     desktopImg.src = projectnumber.desktopimg; 
-//     desktopImg.alt = 'desktopProjectImage';
-//     imgDiv.appendChild(desktopImg);
-//     const mobileImg = document.createElement('img');
-//     mobileImg.className = 'imgA';
-//     mobileImg.src = projectnumber.mobileimg;
-//     mobileImg.alt ='mobileProjectImage';
-//     imgDiv.appendChild(mobileImg);
-//     const projectText = document.createElement('div');
-//     projectText.className = 'projectstext';
-//     projectText.innerHTML = `<h2>${projectnumber.title}</h2>
-//     <ul class="qualities">
-//       <li class="qitem">${projectnumber.made[0]}</li>
-//       <li>
-//         <img src="./images/projectsimages/Counter.png" alt="dotimage" />
-//       </li>
-//       <li class="qitem">${projectnumber.made[1]}</li>
-//       <li>
-//         <img src="./images/projectsimages/Counter.png" alt="dotimage" />
-//       </li>
-//       <li class="qitem">${projectnumber.made[2]}</li>
-//     </ul>
-//     <p>
-//     ${projectnumber.description}
-//     </p>
-//     <ul class="languages">
-//       <li>${projectnumber.languages[0]}</li>
-//       <li>${projectnumber.languages[1]}</li>
-//       <li>${projectnumber.languages[2]}</li>
-//     </ul>
-//     <button type="button" class="button">See Project</button>
-//     `;
-//     projDiv.appendChild(projectText);
-    
-// }
-
-// function createCard2(projectnumber){
-
-//   const projects = document.querySelector('.projectscards');
-//   const projectsCards = document.querySelector('.projectscards');
-//   let projDiv = document.createElement('div');
-//   projDiv.className = 'project';
-//   projectsCards.appendChild(projDiv);
-//   let imgDiv = document.createElement('div');
-//   imgDiv.className = 'img1';
-//   projDiv.appendChild(imgDiv);
-//   const desktopImg = document.createElement('img');
-//   desktopImg.className ='imgAA';
-//   desktopImg.src = projectnumber.desktopimg; 
-//   desktopImg.alt = 'desktopProjectImage';
-//   imgDiv.appendChild(desktopImg);
-//   const mobileImg = document.createElement('img');
-//   mobileImg.className = 'imgA';
-//   mobileImg.src = projectnumber.mobileimg;
-//   mobileImg.alt ='mobileProjectImage';
-//   imgDiv.appendChild(mobileImg);
-//   const projectText = document.createElement('div');
-//   projectText.className = 'projectstext';
-//   projectText.innerHTML = `<h2>${projectnumber.title}</h2>
-//   <ul class="qualities">
-//     <li class="qitem">${projectnumber.made[0]}</li>
-//     <li>
-//       <img src="./images/projectsimages/Counter.png" alt="dotimage" />
-//     </li>
-//     <li class="qitem">${projectnumber.made[1]}</li>
-//     <li>
-//       <img src="./images/projectsimages/Counter.png" alt="dotimage" />
-//     </li>
-//     <li class="qitem">${projectnumber.made[2]}</li>
-//   </ul>
-//   <p>
-//   ${projectnumber.description}
-//   </p>
-//   <ul class="languages">
-//     <li>${projectnumber.languages[0]}</li>
-//     <li>${projectnumber.languages[1]}</li>
-//     <li>${projectnumber.languages[2]}</li>
-//   </ul>
-//   <button type="button" class="button">See Project</button>
-//   `;
-//   projDiv.appendChild(projectText);
-
-// }
-
-
-
-// function createProjects(n,projData){
-// for(let i=1; i<=n; i++){
-//   if(i%2 === 0){
-//     createCard(projData[i]);
-//   }
-//   else createCard2(projData[i]);
-// }
-// };
-
-// createProjects(4,projData);
-
-
-
+document.querySelectorAll(".hamItem").forEach((item) =>
+  item.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("showmenu");
+    bar1.classList.remove("bar1clicked");
+    bar2.classList.remove("bar2clicked");
+    bar3.classList.remove("bar3clicked");
+    hamburger.classList.remove("hamburgerclicked");
+  })
+);
